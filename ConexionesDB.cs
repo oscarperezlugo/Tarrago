b96e79c59d62340040b0458e144f1b36b0f6316b
Edward Rodriguez <edward.rodriguez@alphas.technology>
2020-06-28 12:05:00 -0400

    Tarrago
    Tarrago

---
+++ ConexionesDB.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Tarrago
+{
+    public class ConexionesDB
+    {   
+        public SqlConnection conn = new SqlConnection("workstation id=DESKTOP-BCE3QLS\\SQLEDWARD;user id=sa;pwd=123;data source=DESKTOP-BCE3QLS\\SQLEDWARD;persist security info=False;initial catalog=tarrago");
+        //string conex = "workstation id=Affinity.mssql.somee.com;packet size=4096;user id=operezlugo_SQLLogin_1;pwd=tc65ztfi6o;data source=Affinity.mssql.somee.com;persist security info=False;initial catalog=Affinity";
+        //public DataTable Obtenerinfo(string filtro)
+        //{
+        //    DataTable dt = new DataTable();            
+
+        //    string sqlQuery = "SELECT * FROM EMPRESAS WHERE NOMBRECOMERCIAL Like '%" + filtro + "%'";
+        //    SqlDataAdapter da = new SqlDataAdapter(sqlQuery, conn);
+        //    conn.Open();
+        //    da.Fill(dt);
+        //    conn.Close();
+
+        //    return dt;
+        //}
+
+        //public DataTable Obtenerinfo2(string filtro)
+        //{
+        //    DataTable dt = new DataTable();            
+
+        //    string sqlQuery = "SELECT CEDULA, TRY_CONVERT(datetime, FECHADENACIMIENTO) FECHADENACIMIENTO, NOMBRESCOMPLETOS, GENERO FROM ASEGURADOS WHERE CEDULA Like '%" + filtro + "%'";
+
+        //    SqlDataAdapter da = new SqlDataAdapter(sqlQuery, conn);
+        //    conn.Open();
+        //    da.Fill(dt);
+        //    conn.Close();                       
+        //    return dt;          
+        //}
+
+        //public DataTable getProductos()
+        //{
+        //    DataTable dt = new DataTable();
+
+        //    string sqlQuery = "SELECT * FROM PRODUCTOS";
+
+        //    SqlDataAdapter da = new SqlDataAdapter(sqlQuery, conn);
+        //    conn.Open();
+        //    da.Fill(dt);
+        //    conn.Close();
+        //    return dt;
+        //}
+
+        //public DataSet Obtenerinfo3(filtrovalor filtrorec)
+        //{
+        //    string filtro = filtrorec.filtro;
+        //    string plan = filtrorec.plan;
+        //    string relacion = filtrorec.relacion;
+        //    DataSet ds = new DataSet();
+
+
+        //    //string sqlQuery = "SELECT DISTINCT[Valor] FROM[PRODUCTOS] WHERE(([Producto] LIKE '%" + filtro + "%') AND ([Aplicaa] LIKE '%" + relacion + "%') AND ([Plano] LIKE '%" + plan +"%') AND )";
+        //    string sqlQuery = "SELECT DISTINCT[Valor] FROM[PRODUCTOS2] WHERE(([Producto] LIKE '%" + filtro + "%') AND([Aplicaa] LIKE '%" + relacion + "%') AND([Plano] LIKE '%" + plan + "%')) AND(DATEDIFF(yy, '04-01-1990', GETDATE()) >= Desde AND DATEDIFF(yy, '04-01-1990', GETDATE()) <= Hasta)";
+
+            
+        //    SqlDataAdapter da = new SqlDataAdapter(sqlQuery, conn);
+        //    conn.Open();
+        //    da.Fill(ds);
+        //    conn.Close();
+        //    return ds;
+
+
+        //}
+    }
+}
---
+++ login.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Tarrago
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            string connectionString = "workstation id=DESKTOP-BCE3QLS\\SQLEDWARD;user id=sa;pwd=123;data source=DESKTOP-BCE3QLS\\SQLEDWARD;persist security info=False;initial catalog=tarrago";
+
+            string query = "SELECT nombre_usuario FROM usuario WHERE nombre_usuario=@user AND password=@pwd;";
+
+            // set up a connection and command in using() blocks
+            using (SqlConnection con = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                // add parameters and set their values
+                cmd.Parameters.Add("@user", SqlDbType.VarChar, 50).Value = TextBox1.Text;
+                cmd.Parameters.Add("@pwd", SqlDbType.VarChar, 50).Value = TextBox2.Text;
+
+                // open connection
+                con.Open();
+
+                // establish data reader
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    // if at least one row is returned....  
+                    if (dr.Read())
+                    {
+                        // get employee ID and role from the reader
+                        string nombreUsuario = dr.GetFieldValue<string>(0);
+
+                        Session["bienvenido"] = nombreUsuario;
+                        Session["fecha"] = DateTime.Now.ToString();
+
+                        Response.Redirect("Busqueda.aspx");
+                    }
+                    else
+                    {
+                        string error = "Usuario y Contraseña incorrectos";
+                        //Label1.Text = error;
+
+                        // what do you want to do if NO ROW was returned? E.g. user/pwd combo is wrong
+                    }
+
+                    dr.Close();
+                }
+
+                con.Close();
+            }
+        }
+
+        protected void LinkButtonClicked(object sender, EventArgs e)
+        {
+            //Response.Redirect("RecuperarPass");
+        }
+    }
+}
